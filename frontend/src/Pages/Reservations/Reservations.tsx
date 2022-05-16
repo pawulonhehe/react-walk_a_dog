8ebf26429e739a4770dc4bd2ca4cd392cc32dde1
pkardas <pawelkardas9904@gmail.com>
2022-05-16 16:30:24 +0200

    cos

--- frontend/src/Pages/Reservations/Reservations.tsx
+++ frontend/src/Pages/Reservations/Reservations.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Reservations.scss";
 import { Icon } from "@iconify/react";
 import pudzilla from "../../Assets/Images/pudzilla.jpg";
+<<<<<<< mui
 import { useNavigate } from "react-router-dom";
 
 // import { Link } from "react-router-dom";
@@ -9,6 +10,33 @@ import { useNavigate } from "react-router-dom";
 export const Reservations = () => {
   const navigate = useNavigate();
   const switchToBook = () => navigate("/bookwalk");
+=======
+import BasicModal from "../BasicModal/BasicModal";
+import Popover from "@mui/material/Popover";
+                   
+// import { Link } from "react-router-dom";
+
+export const Reservations = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseP = () => {
+    setAnchorEl(null);
+  };
+
+  const show = Boolean(anchorEl);
+  const id = show ? "simple-popover" : undefined;
+
+>>>>>>> main
   return (
     <div className="Reservations">
       <div className="Reservations--topText">Twoje Rezerwacje</div>
@@ -36,7 +64,10 @@ export const Reservations = () => {
             </div>
             <div className="Reservations--infoButtons">
               <button>Anuluj</button>
-              <button>Szczegóły</button>
+              <button type="button" onClick={handleOpen}>
+                Szczegóły
+              </button>
+              <BasicModal open={open} onClose={handleClose} />
             </div>
           </div>
         </div>
@@ -49,29 +80,29 @@ export const Reservations = () => {
             <Icon icon="material-symbols:calendar-month-outline" />
             Data
           </button>
-          <li className="tooltip">
-            <button className="tooltip">
-              <Icon icon="material-symbols:sound-detection-dog-barking" />
-              Pies
-            </button>
-            <div className="tooltipMenu bottomSide">
-              <ul>
-                <li>
-                  <label>
-                    <input type="checkbox" />
-                    Azor
-                  </label>
-                </li>
-                <li>
-                  <label>
-                    <input type="checkbox" />
-                    Rocky
-                  </label>
-                </li>
-              </ul>
+          <button className="tooltip" onClick={handleClick}>
+            <Icon icon="material-symbols:sound-detection-dog-barking" />
+            Pies
+          </button>
+          <Popover
+            id={id}
+            open={show}
+            anchorEl={anchorEl}
+            onClose={handleCloseP}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+          > 
+            <div className="popover">
+              <label htmlFor="">
+              <input type="checkbox" name="" id="" />
+              Azor
+            </label>
             </div>
-          </li>
-          <button>
+            
+          </Popover>
+          <button className="tooltip">
             <Icon icon="material-symbols:person" />
             Trener
           </button>
@@ -93,7 +124,10 @@ export const Reservations = () => {
                 </div>
               </div>
               <div className="Reservations--infoButtons">
-                <button>Szczegóły</button>
+                <button type="button" onClick={handleOpen}>
+                  Szczegóły
+                </button>
+                <BasicModal open={open} onClose={handleClose} />
                 <button>Oceń</button>
               </div>
             </div>
