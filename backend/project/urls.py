cbd63b406077ce083d5fa248dda0f61aa4a48df0
dl155231 <155231@student.uwm.edu.pl>
2022-06-09 22:23:07 +0200

    backend

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,5 +1,7 @@
 """Project urls."""
 # Django
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
@@ -28,4 +30,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
