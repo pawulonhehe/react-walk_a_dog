7145fbe6a27c702d8d56c65a08bcbb5ce9bdbdd2
pkardas <pawelkardas9904@gmail.com>
2022-06-10 01:09:31 +0200

    ok

---
+++ frontend/src/Components/WalkModalTrainer/WalkModalTrainer.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./WalkModalTrainer.scss";
+import { WalkDetailsTrainer } from "../WalkDetailsTrainer/WalkDetailsTrainer";
+
+export const WalkModalTrainerxd = (props) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div className="WalkModalTrainer">
+      <div className="walk-date">{props.date}</div>
+      <div className="walk-time">{props.start_time + "-" + props.end_time}</div>
+      <div className="walk-trainer">tu psy/trener</div>
+      <div className="walk-details" onClick={handleOpen}>
+        Szczegóły
+      </div>
+      <WalkDetailsTrainer open={open} onClose={handleClose} {...props} />
+    </div>
+  );
+};
