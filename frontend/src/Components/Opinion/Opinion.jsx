87f501314f6b25a04fb1f17a1caf8499259fa3d3
Jolanta Kazmierczak <jolanta.kazmierczak99@gmail.com>
2022-06-10 00:38:22 +0100

    disable button

--- frontend/src/Components/Opinion/Opinion.jsx
+++ frontend/src/Components/Opinion/Opinion.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Opinion.scss";
-import { Rating } from "@mui/material";
+import { Rating, ratingClasses } from "@mui/material";
+import axios from "axios";
 
 export const Opinion = (props) => {
+  const [user, setUser] = useState([]);
+  const token = sessionStorage.getItem("token");
+
+  const userId = props.evaluator;
+  useEffect(() => {
+    axios
+      .get(`/users/${userId}`, {
+        headers: { Authorization: `Token ${token}` },
+      })
+      .then((res) => {
+        sessionStorage.setItem("data", JSON.stringify(res.data));
+        setUser(res.data);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }, []);
+
+
   return (
     <div className="MidContainer--Opinion">
-    <div className="flex">
-      <div className="Opinion__Title">Jan Kowalski</div>
-      <div className="Opinion__Stars">
-        <Rating name="simple-controlled" value="5" />
+      <div className="flex">
+        <div className="Opinion__Title">{user.first_name + " " + user.last_name} </div>
+        <div className="Opinion__Stars">
+          <Rating name="simple-controlled" value={props.value} />
+        </div>
       </div>
-      </div>
-      <div className="Opinion__Article">lorem ipsum cos tam cos</div>
+      <div className="Opinion__Article">{props.comment}</div>
     </div>
   );
 };
