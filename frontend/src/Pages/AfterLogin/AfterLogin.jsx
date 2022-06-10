3cf18621faac529ff4298320a79ca20b66109f92
dl155231 <155231@student.uwm.edu.pl>
2022-06-10 09:25:59 +0200

    adsasd

--- frontend/src/Pages/AfterLogin/AfterLogin.jsx
+++ frontend/src/Pages/AfterLogin/AfterLogin.jsx
@@ -8,24 +8,50 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { WalkModal } from "../../Components/WalkModal/WalkModal";
 import GoogleMapReact from "google-map-react";
+import { WalkModalTrainerxd } from "../../Components/WalkModalTrainer/WalkModalTrainer";
 // import { Link } from "react-router-dom";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export const AfterLogin = () => {
+  const is_trainer = sessionStorage.getItem("is_trainer");
   const navigate = useNavigate();
+  const token = sessionStorage.getItem("token");
+  const [user, setUser] = useState();
+  const [walk, setWalk] = useState([]);
   const switchToMyProfile = () =>
     navigate(`/editaccount/${sessionStorage.getItem("user")}`);
   const switchToMyDogs = () => navigate("/mydogs");
   const switchToReservations = () => navigate("/reservations");
   const switchToTrainers = () => navigate("/trainers");
-  const token = sessionStorage.getItem("token");
-  const [user, setUser] = useState();
-
-  const [imageUrl, setImageUrl] = useState("pudzilla");
-
-
-  console.log(imageUrl)
+  {/* active walks*/}
+  useEffect(() => {
+    if (is_trainer === "true") {
+      axios
+        .get(`/trainers/${sessionStorage.getItem("user")}/active-walks`, {
+          headers: { Authorization: `Token ${token}` },
+        })
+        .then((res) => {
+          sessionStorage.setItem("data", JSON.stringify(res.data));
+          setWalk(res.data);
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    } else {
+      axios
+        .get(`/users/${sessionStorage.getItem("user")}/active-walks`, {
+          headers: { Authorization: `Token ${token}` },
+        })
+        .then((res) => {
+          sessionStorage.setItem("data", JSON.stringify(res.data));
+          setWalk(res.data);
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    }
+  }, []);
 
   useEffect(() => {
     axios
@@ -35,42 +61,119 @@ export const AfterLogin = () => {
       .then((res) => {
         sessionStorage.setItem("data", JSON.stringify(res.data));
         setUser(res.data);
-        setImageUrl(res.data.image ? res.data.image : "pudzilla")
-        console.log("img: ",imageUrl);
       })
       .catch((error) => {
         console.log(error.response);
       });
   }, []);
 
-  const [walk, setWalk] = useState([]);
-  useEffect(() => {
-    axios
-      .get(`/users/${sessionStorage.getItem("user")}/active-walks`, {
-        headers: { Authorization: `Token ${token}` },
-      })
-      .then((res) => {
-        sessionStorage.setItem("data", JSON.stringify(res.data));
-        setWalk(res.data);
-      })
-      .catch((error) => {
-        console.log(error.response);
-      });
-  }, []);
+  if (is_trainer === "false") {
+    return (
+      <div className="AfterLoginsss">
+        {walk.map((walk) => (
+          <WalkModal {...walk} />
+        ))}
+        <div className="AfterLogin">
+          <div className="Avatar">
+            <img src={pudzilla} alt="Pudzilla" width="140" height="140" />
+          </div>
+          <div className="MainContainer">
+            <div className="WelcomeMessage">Witaj {user?.first_name}!</div>
+            <div className="NiceButton" onClick={switchToMyProfile}>
+              <div className="Icon">
+                <Icon
+                  icon="material-symbols:manage-accounts"
+                  style={{ fontSize: "54px" }}
+                />
+              </div>
+              <div className="title">
+                <h3>Mój Profil</h3>
+              </div>
+              <div className="subtitle">
+                Zaaktualizuj swoje dane kontaktowe i inne szczegóły
+              </div>
+            </div>
+            <div className="NiceButton" onClick={switchToMyDogs}>
+              <div className="Icon">
+                <Icon
+                  icon="material-symbols:sound-detection-dog-barking"
+                  style={{ fontSize: "54px" }}
+                />
+              </div>
+              <div className="title">
+                <h3>Moje psy</h3>
+              </div>
+              <div className="subtitle">
+                Dodaj i zaaktualizuj dane swojego zwierzaka
+              </div>
+            </div>
+            <div className="NiceButton" onClick={switchToReservations}>
+              <div className="Icon">
+                <Icon
+                  icon="material-symbols:calendar-month-outline"
+                  style={{ fontSize: "54px" }}
+                />
+              </div>
+              <div className="title">
+                <h3>Rezerwacje</h3>
+              </div>
+              <div className="subtitle">
+                Zobacz swoje nadchodzące usługi lub rezerwuj nowe
+              </div>
+            </div>
+            <div className="NiceButtonb" onClick={switchToTrainers}>
+              <div className="Icon">
+                <Icon
+                  icon="material-symbols:person"
+                  style={{ fontSize: "54px" }}
+                />
+              </div>
+              <div className="title">
+                <h3>Trenerzy</h3>
+              </div>
+              <div className="subtitle">
+                Przeglądaj profile potenclajnych trenerów
+              </div>
+            </div>
+          </div>
+        </div>
+        <div style={{ height: "100vh", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: "AIzaSyAyOgCp9cy7G2rg1uP-00bGEpVNKsZ-eek",
+            }}
+            defaultCenter={{
+              lat: 59.95,
+              lng: 30.33,
+            }}
+            defaultZoom={25}
+          >
+            {/* <div className="cos" lat={59.955413} lng={30.337844}>
+              JD
+            </div> */}
+            <AnyReactComponent
+              lat={59.955413}
+              lng={30.337844}
+              text="My Marker"
+            />
+          </GoogleMapReact>
+        </div>
+      </div>
+    );
+  } else if (is_trainer === "true") {
+    const switchToYourClients = () =>
+      navigate(`/yourclients/${sessionStorage.getItem("user")}`);
+    const switchToStartWork = () => navigate("/startwork");
+    const switchToMyOpinions = () =>
+      navigate(`/myopinions/${sessionStorage.getItem("user")}`);
 
-  
-  // console.log(walk);
-  // let imageUrl = user.image ? user.image : "pudzilla"
-  console.log("imgafter: ",imageUrl);
-
-  return (
-    <div className="AfterLoginsss">
-      {walk.map((walk) => (
-        <WalkModal {...walk} />
-      ))}
+    return (
       <div className="AfterLogin">
+        {walk.map((walk) => (
+          <WalkModalTrainerxd {...walk} />
+        ))}
         <div className="Avatar">
-          <img src={imageUrl} alt="Pudzilla" width="140" height="140" />
+          <img src={pudzilla} alt="Pudzilla" width="140" height="140" />
         </div>
         <div className="MainContainer">
           <div className="WelcomeMessage">Witaj {user?.first_name}!</div>
@@ -88,21 +191,7 @@ export const AfterLogin = () => {
               Zaaktualizuj swoje dane kontaktowe i inne szczegóły
             </div>
           </div>
-          <div className="NiceButton" onClick={switchToMyDogs}>
-            <div className="Icon">
-              <Icon
-                icon="material-symbols:sound-detection-dog-barking"
-                style={{ fontSize: "54px" }}
-              />
-            </div>
-            <div className="title">
-              <h3>Moje psy</h3>
-            </div>
-            <div className="subtitle">
-              Dodaj i zaaktualizuj dane swojego zwierzaka
-            </div>
-          </div>
-          <div className="NiceButton" onClick={switchToReservations}>
+          <div className="NiceButton" onClick={switchToMyOpinions}>
             <div className="Icon">
               <Icon
                 icon="material-symbols:calendar-month-outline"
@@ -110,44 +199,37 @@ export const AfterLogin = () => {
               />
             </div>
             <div className="title">
-              <h3>Rezerwacje</h3>
+              <h3>Opinie</h3>
             </div>
             <div className="subtitle">
-              Zobacz swoje nadchodzące usługi lub rezerwuj nowe
+              Zobacz opinie wystawione przez klientów na twój temat
             </div>
           </div>
-          <div className="NiceButtonb" onClick={switchToTrainers}>
+          <div className="NiceButton" onClick={switchToYourClients}>
             <div className="Icon">
               <Icon
-                icon="material-symbols:person"
+                icon="material-symbols:account-circle"
                 style={{ fontSize: "54px" }}
               />
             </div>
             <div className="title">
-              <h3>Trenerzy</h3>
+              <h3>Klienci</h3>
             </div>
             <div className="subtitle">
-              Przeglądaj profile potenclajnych trenerów
+              Przeglądaj profile potencjalnych klientów{" "}
             </div>
           </div>
+          <button
+            type="submit"
+            className="startWork"
+            onClick={switchToStartWork}
+          >
+            Zacznij pracę!
+          </button>
+          {/* <div className="sloty"><Slots /></div> */}
         </div>
       </div>
-      <div style={{ height: "100vh", width: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyAyOgCp9cy7G2rg1uP-00bGEpVNKsZ-eek" }}
-          defaultCenter={{
-            lat: 59.95,
-            lng: 30.33,
-          }}
-          defaultZoom={25}
-        >
-          {/* <div className="cos" lat={59.955413} lng={30.337844}>
-            JD
-          </div> */}
-          <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
-        </GoogleMapReact>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 // AIzaSyAyOgCp9cy7G2rg1uP-00bGEpVNKsZ-eek
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import "./Home.scss";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { Navigate } from "react-router-dom";
 export const Home = () => {
   const handleLogout = () => {
     axios.post("auth/logout/").then(() => {
@@ -9,41 +9,28 @@ export const Home = () => {
       window.location.reload();
     });
   };
-
-  if (sessionStorage.getItem("token"))
-    return (
-      <div className="home">
-        <div className="home__container">
-          <div className="home__container__title">
-            <h1>Welcome to the Home Page</h1>
-            <h2>Dashboard</h2>
-          </div>
-          <div>
-            <button onClick={handleLogout}>Logout</button>
-          </div>
+  if (sessionStorage.getItem("token") !== null) {
+    window.location.reload();
+  }
+  return (
+    <div className="home">
+      <div className="hometitle--container">
+        <div className="hometitle">
+          <h1>Walk-A-Dog</h1>
         </div>
       </div>
-    );
-  else
-    return (
-      <div className="home">
-        <div className="hometitle--container">
-          <div className="hometitle">
-            <h1>Walk-A-Dog</h1>
-          </div>
-        </div>
-        <div className="buttoncontainer">
-          <Link to="login">
-            <button type="submit" className="button">
-              Zaloguj
-            </button>
-          </Link>
-          <Link to="register">
-            <button type="submit" className="button">
-              Zarejestruj
-            </button>
-          </Link>
-        </div>
+      <div className="buttoncontainer">
+        <Link to="login">
+          <button type="submit" className="button">
+            Zaloguj
+          </button>
+        </Link>
+        <Link to="register">
+          <button type="submit" className="button">
+            Zarejestruj
+          </button>
+        </Link>
       </div>
-    );
+    </div>
+  );
 };
