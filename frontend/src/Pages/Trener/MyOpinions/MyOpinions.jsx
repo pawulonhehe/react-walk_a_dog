c08174ab5249b7f753d72a7181db3e5d8a36d507
Jolanta Kazmierczak <jolanta.kazmierczak99@gmail.com>
2022-06-10 01:02:43 +0100

    opinion list for trainer

--- frontend/src/Pages/Trener/MyOpinions/MyOpinions.jsx
+++ frontend/src/Pages/Trener/MyOpinions/MyOpinions.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
 import "./MyOpinions.scss";
 import { Opinion } from "../../../Components/Opinion/Opinion";
+import axios from "axios";
+
+
 
 export const MyOpinions = () => {
+
+  const userId = sessionStorage.getItem("user");
+  const token = sessionStorage.getItem("token");
+  const [trainerReview, setTrainerReview] = useState([]);
+
+
+  useEffect(() => {
+    axios
+      .get(`/trainers/${userId}/reviews/`, {
+        headers: { Authorization: `Token ${token}` },
+      })
+      .then((res) => {
+        sessionStorage.setItem("data", JSON.stringify(res.data));
+
+        const ratingData = res.data;
+
+
+        setTrainerReview(ratingData);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }, []);
+
   return (
     <div className="MyOpinions">
       <div className="MyOpinions--title">Moje opinie</div>
@@ -10,7 +38,10 @@ export const MyOpinions = () => {
         zobaczysz tutaj wystawione przez klientów opinie na swój temat
       </div>
       <div className="MyOpinions--container">
-        <Opinion />
+        {trainerReview.map( review => (
+            <Opinion {...review}/>
+        ))}
+        
       </div>
     </div>
   );
